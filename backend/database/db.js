13e63e774599a84babd78b1a10bb07c99ea8aac4
Rithk <rithkchaudhary86@gmail.com>
2024-04-07 21:51:14 +0530

    database done

---
+++ backend/database/db.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://naruto:jVSNMMGwfSVPzHXV@cluster0.78poq1d.mongodb.net/SchoolManagementSystem');
+
+const student= new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    rollno: Number,
+    mobileno: Number
+
+})
+const teacher= new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    rollno: Number,
+    mobileno: Number
+
+})
+const schoolData= new mongoose.Schema({
+    class: String,
+    subjects: {
+        sub1: String,
+        sub2: String,
+        sub3: String,
+        sub4: String,
+        sub5: String,
+    }
+
+})
+
+const Student = mongoose.model("Student",student);
+const Teacher = mongoose.model("Teacher",teacher);
+const SchoolData = mongoose.model("SchoolData",schoolData);
+
+module.exports={
+    Student,
+    Teacher,
+    SchoolData
+}
