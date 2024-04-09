5e62c8b5afdd0494593289fcb6e7e58fec1e0f59
Rithk <rithkchaudhary86@gmail.com>
2024-04-09 19:14:25 +0530

    front-end-setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Heading } from "./components/Heading";
+
+function App(){
+  return(
+    <>
+    <Heading label={"Signup"} />
+    </>
+  )
+}
+
+export default App;
